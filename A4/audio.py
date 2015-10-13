401c026c9768a5ddc3de8dc55cd5def27b4751ef
Brij Shah <brijshah0@gmail.com>
2015-10-12 23:12:00 -0700

    cleaned up files, these are some weird method names..

--- A4/audio.py
+++ A4/audio.py
@@ -1,75 +1,142 @@
-import wave, argparse
+#!/usr/bin/python
 
-#-----------------------------------------------
-#-- Source File:	audio.py - reverse audio
-#
-#-- Programmers:	Brij Shah & Callum Styan
-#
-#-- Date:			October 4, 2015
-#-----------------------------------------------
+#-----------------------------------------------------------------------------
+#--	SOURCE FILE:	audio.py -   audio song generator
+#--
+#--	FUNCTIONS:		createBackground(song)
+#--					prettyGirls(song)
+#--					ohYeah(song)
+#--					mars(song)					
+#--					drums(song)
+#--					delete(song)
+#--					main()															
+#--
+#--	DATE:			October 12, 2015
+#--
+#--	PROGRAMMERS:	Brij Shah & Callum Styan
+#--
+#--	NOTES:
+#--	
+#-----------------------------------------------------------------------------
 
-parser = argparse.ArgumentParser(description='Reverse Audio')
-parser.add_argument('-o', '--output', dest='outputFile', help='the output file', required=True)
-parser.add_argument('-i', '--input', dest='inputFile', help='the input file', required=True)
-parser.add_argument('-u', '--speedup', dest='speedUp', help='fast forward the file')
-parser.add_argument('-d', '--slowdown', dest='slowDown', help='slow down the file')
-parser.add_argument('-r', '--reversal', action="store_true", help='play audio reversed')
-args = parser.parse_args()
+import wave, argparse, warnings, os
+from pydub import AudioSegment
 
-if args.speedUp and args.slowDown:
-	print "Sorry, you can't speed up and slow down.  Please only set one of those flags"
-	exit()
+# globals
+background = None
+prettyGirls = None
+ohyeahs = None
+marsOyeah = None
+girlsPretty = None
 
+# open song
+song = AudioSegment.from_wav('bspears.wav')
+
+# create background beat
+def createBackground(song):
+	global background
+	background = song[2600:5000]
+	background = background.append(background)
+	background = background * 50
+	background_reverse = background.reverse()
+	background_reverse = background_reverse
+	background_reverse = background_reverse.apply_gain(-25.0)
+	background = background.overlay(background_reverse)
+	background = background.fade_in(8800).fade_out(8800)
+	background.export('background.wav', 'wav')
+
+# pretty girls segment
+def prettyGirls(song):
+	global prettyGirls
+	prettyGirls = song[12050:14800]
+	prettyGirls = prettyGirls.fade_in(1500)
+
+# oh yeah segment
+def ohYeah(song):
+	global oyeahs
+	oyeah = song[121800:122400]
+	quietest = oyeah.apply_gain(-20)
+	quieter = oyeah.apply_gain(-12.5)
+	quiet = oyeah.apply_gain(-5)
+	oyeahs = oyeah.append(quiet).append(quieter).append(quietest)
+	oyeahs.export('oyeah.wav', 'wav')
+
+# mars
+def mars(song):
+	global marsOyeah
+	silent5 = AudioSegment.silent(duration=22000)
+	mars = song[78000:80500]
+	mars.export('mars.wav', 'wav')
+	mars = wave.open('mars.wav', 'r')
+	marsFast = wave.open('marsFast.wav', 'w')
+	marsFast.setparams(mars.getparams())
+	writeFrames = mars.readframes(mars.getnframes())
+	marsFast.setframerate(int(round(mars.getframerate() * 1.25)))
+	marsFast.writeframes(writeFrames)
+	marsFast.close()
+	marsFast = AudioSegment.from_wav('marsFast.wav')
+	marsOyeah = silent5.append(marsFast).append(oyeahs).append(silent5)
+
+# drum beat
+def drums(song):
+	global girlsPretty
+	silentStart = AudioSegment.silent(duration=13200)
+	smallSilent = AudioSegment.silent(90)
+	drums = song[196090:198400]
+	drums = drums.append(smallSilent)
+	drums = drums.overlay(drums[500:1000], position = 1300)
+	drums = drums * 45
+	drums = drums.apply_gain(-5)
+	drums = silentStart.append(drums)
+	output = background.overlay(drums).overlay(marsOyeah, times=3).overlay(girlsPretty, position=35200, times=3)
+	output.export('output.wav', 'wav')
+
+# delete unused files
+def delete():
+	os.remove('background.wav')
+	os.remove('mars.wav')
+	os.remove('marsFast.wav')
+	os.remove('oyeah.wav')
+	os.remove('soPretty.wav')
+	os.remove('soPrettySlow.wav')
+
+# pseudomain
 def main():
-	#open wave file for reading
-	w = wave.open(args.inputFile, 'r')
+	global background
+	global prettyGirls
+	global oyeahs
+	global marsOyeah
+	global girlsPretty
 
-	#open wave file for writing to
-	openWaveFile = wave.open(args.outputFile, 'w')
+	createBackground(song)
+	prettyGirls(song)
 
-	#returns sample width in bytes
-	sampRate = w.getsampwidth()
+	# we just so pretty
+	soPretty = song[19990:21250]
+	soPretty.export('soPretty.wav', 'wav')
+	soPretty = wave.open('soPretty.wav', 'r')
+	soPrettySlow = wave.open('soPrettySlow.wav', 'w')
+	soPrettySlow.setparams(soPretty.getparams())
+	writeFrames = soPretty.readframes(soPretty.getnframes())
+	soPrettySlow.setframerate(soPretty.getframerate() / 2)
+	soPrettySlow.writeframes(writeFrames)
+	soPrettySlow.close()
+	soPrettySlow = AudioSegment.from_wav('soPrettySlow.wav')
 
-	#returns sampling frequency
-	frameRate = w.getframerate()
+	#combine last two
+	silent5 = AudioSegment.silent(duration=22000)
+	smallSilent = AudioSegment.silent(90)
+	girlsPretty = prettyGirls.append(smallSilent).append(soPrettySlow).append(silent5)
 
-	#sets the frame rate to n
-	openWaveFile.setframerate(frameRate)
-
-	#set sample width to n bytes
-	openWaveFile.setsampwidth(sampRate)
-
-	#returns number of audio channels(1 for mono 2 for stereo)
-	channels = w.getnchannels()
-
-	#set the number of channels
-	openWaveFile.setnchannels(channels)
-
-	#empty string
-	writeframes = ""
-
-	if args.reversal == True:
-		#loop through frames and assign each frame to the reversedframe variable
-		for i in range(w.getnframes()):
-			frame = w.readframes(1)
-			writeframes = frame + writeframes
-	else:
-		writeframes = w.readframes(w.getnframes())
-
-	#write reversed frames to wav file
-	if args.speedUp is not None:
-		openWaveFile.setframerate(int(round(openWaveFile.getframerate() * float(args.speedUp))))
-	elif args.slowDown is not None:
-		openWaveFile.setframerate(int(round(openWaveFile.getframerate() / float(args.slowDown))))
-	openWaveFile.writeframes(writeframes)
+	ohYeah(song)
+	mars(song)
+	drums(song)
+	delete()
 
 if __name__ == '__main__':
 	try:
 		main()
 	except KeyboardInterrupt:
-		print "Exiting.."
-	except IOError:
-		print "file not found, try again.."
-
-
+		print "exiting.."
+	
 
